7ca9f848232a6e0f61cb2cb8c37ee6742ff5ce85
Jing <jhu8187@gmail.com>
2023-05-24 12:27:17 -0700

    remove duplicate main files

--- task.py
+++
@@ -1,9 +0,0 @@
-# properties of a task
-
-class task:
-    def __init__(self, desc=None, is_complete=False):
-        self.desc=desc
-        self.is_complete=is_complete
-
-    def print_desc(self):
-        print(self.desc)
